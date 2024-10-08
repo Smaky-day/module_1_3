4e0c8f4f6703836e67b1da5b86b8cc6b7a8ec43b
Smaky-day <05102024smoky@gmail.com>
2024-10-08 14:15:09 +0400

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Lana'
+print('Name: ' + name)
+age = 27
+print('Age: ' + str(age))
+age = age + 3
+print('New Age: ' + str(age))
+is_student = True
+print('Is Student: ' + str(is_student))
+
